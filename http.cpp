386f698816a443a720a50c34f9f7e6908200bfe8
Franco Speziali <developer@wemakemachines.com>
2025-03-13 20:51:56 +0100

    Add http.h, http.cpp

---
+++ http.cpp
@@ -0,0 +1,25 @@
+#include "http.h"
+
+#include <HTTPClient.h>
+
+String httpPOSTRequest(const char* url, String json) {
+  HTTPClient http;
+  String jsonResponse = "{}";
+
+  http.begin(url);
+  http.addHeader("Content-Type", "application/json");
+  int httpResponseCode = http.POST(json);
+
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    jsonResponse = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
+
+  return jsonResponse;
+}
---
+++ http.h
@@ -0,0 +1,8 @@
+#ifndef HTTP_H
+#define HTTP_H
+
+#include <Arduino.h>
+
+String httpPOSTRequest(const char* url, String json);
+
+#endif
